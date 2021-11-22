1d7306630f75aa33178ddd600d70b6b545a8b5e5
dionisimus <regserv@tuta.io>
2021-11-22 12:23:02 +0200

    finnished 02_01 part1 Implementing cipher

--- java-programming/Java Programming Arrays, Lists, and Structured Data/02_cryptography-keeping-information-secret/01_implementing-the-caesar-cipher/Part1/CaesarCipher.java
+++ java-programming/Java Programming Arrays, Lists, and Structured Data/02_cryptography-keeping-information-secret/01_implementing-the-caesar-cipher/Part1/CaesarCipher.java
@@ -38,14 +38,46 @@ public class CaesarCipher
         System.out.println("key is " + key + "\n" + encrypted);
     }
     
+    public char Encryption (int key, char ch){
+        String Alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String encrAlph1 = Alph.substring(key) + Alph.substring(0,key);
+        int findInt = 0;
+        char newChar = 0;
+        if (Character.isLetter(ch)){
+            if (Character.isUpperCase(ch)){
+                    findInt = Alph.indexOf(ch);
+                    if (findInt != -1){
+                        newChar = encrAlph1.charAt(findInt);
+                        
+                    }
+                }
+            if (Character.isLowerCase(ch)){
+                    findInt = Alph.indexOf(Character.toUpperCase(ch));
+                    if (findInt != -1){
+                        newChar = encrAlph1.charAt(findInt);
+                        newChar = Character.toLowerCase(newChar);
+                        
+                    }
+                }
+        return newChar;
+        }
+        else{
+        return ch;
+        }
+    }
+
     public String encryptTwoKeys (String input, int key1, int key2){
         StringBuilder replaced = new StringBuilder(input);
+        char findChar;
         for (int i = 0; i < replaced.length(); i++){
-            if ((i+1) % 2 == 0)
-                encrypt(input, key2);
-            if ((i+1) % 2 != 0)
-                encrypt(input, key1);
+            findChar = replaced.charAt(i);
+            if (i % 2 == 0)
+                replaced.setCharAt(i,Encryption(key1,findChar));
+                
+            if (i % 2 != 0)
+                replaced.setCharAt(i,Encryption(key2,findChar));
         }
+        System.out.println(replaced.toString());
         return replaced.toString();
     }
 }
