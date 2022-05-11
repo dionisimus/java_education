9c230abc6c5589954a7c709f9d43d5ecf01f6b04
dionisimus <regserv@tuta.io>
2022-05-11 18:31:27 +0300

    Started Course

---
+++ java-programming/Advanced Data Structures in Java/Advanced-Data-Structures-in-Java-master/src/gmapsfx/javascript/object/GMapObjectType.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright 2014 Lynden, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package gmapsfx.javascript.object;
+
+/**
+ * @author Rob Terpilowski
+ */
+public interface GMapObjectType {
+
+    String OBJECT = "Object";
+    String MAP = "google.maps.Map";
+    String INFO_WINDOW = "google.maps.InfoWindow";
+    String LAT_LNG = "google.maps.LatLng";
+    String MARKER = "google.maps.Marker";
+    String SIZE = "google.maps.Size";
+    String POLYLINE = "google.maps.Polyline";
+    String POLYGON = "google.maps.Polygon";
+    String RECTANGLE = "google.maps.Rectangle";
+    String CIRCLE = "google.maps.Circle";
+    String MVC_ARRAY = "google.maps.MVCArray";
+    String LAT_LNG_BOUNDS = "google.maps.LatLngBounds";
+    String ELEVATION_SERVICE = "google.maps.ElevationService";
+    String ELEVATION_RESULT = "google.maps.ElevationResult";
+    String MAX_ZOOM_SERVICE = "google.maps.MaxZoomService";
+    String MAX_ZOOM_RESULT = "google.maps.MaxZoomResult";
+    String PROJECTION = "google.maps.Projection";
+    String POINT = "google.maps.Point";
+
+}
